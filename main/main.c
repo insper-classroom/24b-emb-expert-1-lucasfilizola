4f985f4d1091812dd5da3348764e77e029372d9a
lucasfilizola <lucasfpp@al.insper.edu.br>
2024-11-01 16:12:24 -0300

    ola

--- main/main.c
+++ main/main.c
@@ -7,11 +7,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "servo.h"
 
-int main() {
+
+
+bool direction = true;
+int currentMillis = 400;
+int servoPin = 0; 
+
+int main()
+{
     stdio_init_all();
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    setServo(servoPin, currentMillis);
+    while (true)
+    {
+        currentMillis += (direction)?5:-5;
+        if (currentMillis >= 2400) direction = false;
+        if (currentMillis <= 400) direction = true;
+        setMillis(servoPin, currentMillis);
+        sleep_ms(10);
     }
 }
